1c81425c050b3520ba9739504b694111152a4013
Daniel Dinev <azsumbg@gmail.com>
2025-04-27 10:57:46 +0300

    Release ver 1.0

--- Zombie wars/Zombie wars.cpp
+++ Zombie wars/Zombie wars.cpp
@@ -1736,6 +1736,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 
     CreateResources();
 
+    PlaySound(sound_file, NULL, SND_ASYNC | SND_LOOP);
+
     while (bMsg.message != WM_QUIT)
     {
         if ((bRet = PeekMessage(&bMsg, bHwnd, NULL, NULL, PM_REMOVE)) != 0)
